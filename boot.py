bd7ad4c2dae94454712d4729fdc6d899dbf64173
Achton Smidt Winther <achton@reload.dk>
2017-06-05 21:38:41 +0200

    Add support for configuring the ESP with static IP.

--- boot.py
+++ boot.py
@@ -16,6 +16,13 @@ def do_connect():
         a_if.active(False)
     if not s_if.isconnected():
         s_if.active(True)
+        # Set static IP if configured.
+        try:
+            s_if.ifconfig((config.IP, config.SUBNET, config.GATEWAY, config.DNS))
+        except:
+            if not config.SILENT:
+                print("Static IP not configured. Setting IP via DHCP.")
+        # Connect to Wifi.
         s_if.connect(secrets.WIFI_SSID, secrets.WIFI_PASSPHRASE)
         while not s_if.isconnected():
             pass
--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 # If set, writing to serial console will be minimized
 SILENT = True
+
+# Set the values below to initiate the network interface with a static IP.
+#IP = "192.168.0.123"
+#SUBNET = "255.255.255.0" # aka Netmask
+#GATEWAY = "192.168.0.1"
+#DNS = "8.8.8.8"
